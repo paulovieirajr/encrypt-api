a8b8832f250134f8e0bf635e50955325a2740293
Junior Vieira <junior.vieira.1990@gmail.com>
2022-10-17 16:25:06 -0300

    :recycle: refactor: Removed parameter that exclude SecurityAutoConfiguration

--- src/main/java/br/dev/paulovieira/encrypt/EncryptApplication.java
+++ src/main/java/br/dev/paulovieira/encrypt/EncryptApplication.java
@@ -2,9 +2,8 @@ package br.dev.paulovieira.encrypt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class EncryptApplication {
 
     public static void main(String[] args) {
